b44b13d3b2e4fcb4e1629957f6ab3638b8ad3985
Anayk24 <anaykhandelwal26@gmail.com>
2026-02-06 18:23:33 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,18 @@
+import java.io.Serializable;
+
+class Employee implements Serializable {
+    private int id;
+    private String name;
+    private double salary;
+
+    public Employee(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee [ID=" + id + ", Name=" + name + ", Salary=" + salary + "]";
+    }
+}
